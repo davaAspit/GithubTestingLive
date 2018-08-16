614f42450b40183a4aea0beb5661a8967995d34f
Daniel Valsby-Koch <dava@aspit.dk>
2018-08-16 09:49:31 +0200

    Added a goodbye statement to the code

--- GithubTesting/Program.cs
+++ GithubTesting/Program.cs
@@ -11,6 +11,7 @@ namespace GithubTesting
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Github");
+            Console.WriteLine("Goodbye");
         }
     }
 }
